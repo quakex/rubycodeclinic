ff5331bc4afaf45d13e6a9e9bbe1b1671bc7f0a3
quakex <iquakex@gmail.com>
2016-10-03 18:48:11 +0800

    finish date validate

--- functions.rb
+++ functions.rb
@@ -1,15 +1,26 @@
 require 'readline'
 require 'date'
 
+DATA_START_DATE = '2006-09-20'
+
+MAX_DAYS = 7
+
 def query_user_for_date_range
   start_date = nil
   end_date = nil
+  
+  until start_date && end_date
+    puts "\nFirst, we need a start date."
+    start_date = query_user_for_date
 
-  puts "\nFirst, we need a start date."
-  start_date = query_user_for_date
-
-  puts "\nNext, we need an end date."
-  end_date = query_user_for_date
+    puts "\nNext, we need an end date."
+    end_date = query_user_for_date
+    
+    if !date_range_valid?(start_date, end_date)
+      puts "Let's try again."
+      start_date = end_date = nil
+    end
+  end
 
   return start_date, end_date
 end
@@ -28,6 +39,30 @@ def query_user_for_date
     rescue ArgumentError
       puts "\nInvalid date format"
     end
+
+    date = nil unless date_valid?(date)
   end
   return date 
 end
+
+def date_valid?(date)
+  valid_dates = Date.parse(DATA_START_DATE)..Date.today
+  if valid_dates.cover?(date)
+    return true
+  else
+    puts "\nDate must be after #{DATA_START_DATE} and before today."
+    return false
+  end
+end
+
+def date_range_valid?(start_date, end_date)
+  if start_date > end_date
+    puts "\nStart date must be before end date."
+    return false
+  elsif start_date + MAX_DAYS < end_date
+    puts "\nNo more than #{MAX_DAYS} days. Be kind to the remote server!"
+    return false
+  end
+  return true
+end
+
